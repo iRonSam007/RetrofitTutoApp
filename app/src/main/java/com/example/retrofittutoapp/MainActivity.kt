b298f21ead5ed2d73fccf44962e526bff7d147fa
iRonSam007 <abida.invest10@gmail.com>
2024-10-06 22:55:43 +0200

    Adding activity layout

--- app/src/main/java/com/example/retrofittutoapp/MainActivity.kt
+++ app/src/main/java/com/example/retrofittutoapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RetrofitTutoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android less Acti",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
